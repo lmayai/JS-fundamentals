9e4a03706fbd4de90cc39bbd038a67a5b382ecac
Insite <insite@insite-leo-pc.insite.local>
2019-04-04 18:05:13 -0500

    Clase 1. Variables

---
+++ clase1-var.js
@@ -0,0 +1,17 @@
+console.log('Impresion en consola');
+
+var nombre;             //Es una variable undefined
+nombre = 'Leoncito'     //DEbido al contenido la variable se define como String
+                        // Usar ' ' o " " es lo mismo
+
+var apellido = 'Maya'
+console.log('Hola ' + nombre + ' ' + apellido);
+
+//JS debilmente tipado : EL tipo no es definido
+var edad = 20 // Empezo como entero
+var edad = '20 años' //Pasó a ser numero
+console.log('Tengo: ' + edad)
+
+var peso = 75
+peso = '75 kg'
+
