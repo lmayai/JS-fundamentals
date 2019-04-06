d8e26c383bd8e1c43253a5e78000f3fe94b5320d
lmayai <leonardomayairegui@gmail.com>
2019-04-06 16:46:12 -0500

    Numeros en JS

---
+++ clase3-numeros.js
@@ -0,0 +1,37 @@
+var edad = 27;
+
+//Incrementos
+edad = edad + 1;
+edad += 1;
+
+//Decrementos
+var peso = 75;
+peso = peso - 2;
+peso -= 2;
+
+//EJm
+var sandwich = 1;
+peso = peso + sandwich; //Suma
+var jugar = 3;
+peso -= jugar;
+
+//DEcimales
+var precioVino = 200.3;
+var totalx3 = precioVino * 3; //600.9000000000001 -> LA menra de almacenar decimales no es precisa.
+    //Para corregir;
+var totalx3Correcto = Math.round( precioVino*3*100 ) /100; //
+
+// Redondeo completo
+var totalx3Correcto2 = Math.round(totalx3)
+
+// 2 decimales
+var total2DecimalesStr = totalx3.toFixed(2) //Pero esto es un string
+
+// Convertir String a Numero
+var total2DecimalesNum = parseFloat(total2DecimalesStr) 
+var total2DecimalesNum2 = Number(total2DecimalesStr)
+
+//División
+var pizza = 8;
+var personas =2;
+var cantPorcionesPersona = pizza / personas; 
