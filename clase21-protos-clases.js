0459167a282f7e1b420654ff9715481c72e45367
lmayai <leonardomayairegui@gmail.com>
2019-04-11 19:29:27 -0500

    Modificando prototipos

---
+++ clase21-protos-clases.js
@@ -0,0 +1,80 @@
+/**
+ * cUANDO SE HABLA DE OBJ EN JS SE HABLA MAS DE PROTOTIPOS QUE DE CLASES.
+ * Las clases terminan siendo prototipos
+ * 1. primero se definira un protiipo
+ * 2. el objeto se crea usando new, segun el prototipo
+ * 3. this hace referencia al objeto que se acaba de construir
+ * 4. Ese ojeto this que se crea no tiene atributos, por tal se llenan
+ * 5. implicitamente se retorna el this
+ * 6. Para implementar la funcion saludar, se le puede decir al prototipo
+ *    DEntro del proto, existe un atributo saludar, que será una funcion   
+ */
+
+function Persona(nombre,apellido){
+    console.log('Me ejecutaron')
+    this.nombre = nombre
+    this.apellido =apellido
+    this.edad = 20 //Todos los objetos persona dentran 20
+    //implicitamente se retorna el this return this.
+}
+
+Persona.prototype.saludar = function () {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+}
+
+/**
+ * New es una palabra reservada para crear nuevos objetos pasados en prototipos
+ * La palabra new 
+*/ 
+var sacha = new Persona('Sacha','Diaz')
+sacha.saludar()
+
+//CAda uno de estos guarda dentro de this esos valores
+var erika = new Persona('Erika','Gomez')
+var pedro = new Persona('Pedro','Aguirre')
+
+//******************************************++ */
+/**
+ * Sin usar new, se crea un objeto y se asigna a los atributos de un objetp
+ * ESTO ESTA MAL ESCRITO, po mas que funcione
+ */
+function Persona2(nombre,apellido){
+    obj ={}
+    obj.nombre = nombre
+    obj.apellido =apellido
+    return obj
+}
+var sacha2 = Persona2('Sacha','Diaz')
+
+//RETO: Agregar el atributo altura dentro del constructor y 
+// agregar la funcion de decir si es alta o no, mas de 1.8 metros
+
+function Persona3(nombre,apellido,altura){
+    this.nombre= nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+
+Persona3.prototype.saludar = function() {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+}
+
+const esAlto = ({altura}) => altura >=1.8 
+
+Persona3.prototype.mensajeAltura = function() {
+    if (esAlto(this)){
+        console.log(`SI soy alto, mido ${this.altura} metros`)
+    }else{
+        console.log(`NO soy alto, mido ${this.altura} metros`)
+    }
+}
+
+var david = new Persona3('David','Maya',1.7)
+var carlos = new Persona3('Carlos','Maya',1.6)
+var andres = new Persona3('Andres','Maya',1.9)
+var camilo = new Persona3('Camilo','Maya',2)
+
+david.mensajeAltura()
+carlos.mensajeAltura()
+andres.mensajeAltura()
+camilo.mensajeAltura()
---
+++ clase22-modificando-protos.js
@@ -0,0 +1,42 @@
+/**
+ * Seguir viendo diferencias con el objeto normal 
+ * de clase de otros lenguajes
+ * El prototipo es un objeto mas de JS, si se modifica
+ */
+
+function Persona(nombre,apellido,altura){
+    this.nombre = nombre
+    this.apellido =apellido
+    this.altura =altura
+}
+
+// Aqui es donde se modifica el prototipo
+Persona.prototype.saludar = function () {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+}
+
+/**
+ * Esto no puede escribirse como un arrow functions
+ * Ya que el this para estas es otras cosas, OJO CON ESTO
+ */
+Persona.prototype.soyAlto1 = () => this.altura >=1.8
+Persona.prototype.soyAlto2 = function () {
+    return this.altura >=1.8
+}
+
+var sacha = new Persona('Sacha','Diaz',1.72)
+var erika = new Persona('Erika','Gomez',1.65)
+var pedro = new Persona('Pedro','Aguirre',1.8)
+
+sacha.soyAlto1()
+erika.soyAlto1()
+pedro.soyAlto1()
+
+/**
+ * Al ejecutar esto se tendra un error, ya que importa donde se 
+ * colque las funciones de los protos, por tal las funciones se poenen juntas
+ * Por tal los proroipos se ponen arriba y no abajo.
+ */
+Persona.prototype.soyAlto = function() {
+    return this.altura >=1.8
+}
