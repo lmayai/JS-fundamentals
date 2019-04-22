88e88809398905cdb399bf390e9f95b3df9b6c03
lmayai <leonardomayairegui@gmail.com>
2019-04-22 11:26:24 -0500

    Se agrega JQUERY

---
+++ clase29-callbacks.js
@@ -0,0 +1,9 @@
+/**
+ * CALLBACKS!
+ * 
+ * 1. Usar requests a una API externa usando JQUERY
+ * 2. Las APIS van a STARWRAS
+ * 3. Se necesita descargar JQUERY, usarlo con un CDN(un content delivery network: servidor que tramitará el request)
+ * 
+ */
+
