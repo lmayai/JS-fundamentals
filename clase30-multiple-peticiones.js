f61fdc6bf17d6b38a2b03282e4739ef9bfe195bc
lmayai <leonardomayairegui@gmail.com>
2019-04-22 13:06:47 -0500

    Múltiples peticiones asincrónicas

---
+++ clase30-multiple-peticiones.js
@@ -0,0 +1,32 @@
+/**
+ * Programa que imprima multiples personajes, y que esos request sean en paralelo
+ */
+
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+
+const options ={ crossDomain:true }
+
+const onPeopleResponse = function (persona){
+    console.log(`FUN: Hola yo soy ${persona.name}`)
+}
+
+function obtenerPersonaje(id){
+    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+    $.get(url , options , onPeopleResponse)
+}
+
+obtenerPersonaje(1) // Luke
+obtenerPersonaje(2) // R2-D2
+obtenerPersonaje(3) // C-3PO
+obtenerPersonaje(4) 
+obtenerPersonaje(5) 
+obtenerPersonaje(6)
+
+/** El orden en que llegan es: ALEATORIO 
+ * No depende del orden de llamada, debido al asincronismo
+ * ya que no es posible saber cuando respondera.
+ * El asincronismo es evidente debido a la respuesta del server que es desconocida
+ * Por tal el orden en que se pide no depende del orden que llega
+*/
+
