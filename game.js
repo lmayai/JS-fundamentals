a21a994fef3f295493a1016d87ebdbd0a592ef10
lmayai <leonardomayairegui@gmail.com>
2019-04-24 13:05:52 -0500

    Generando secuencia de colores con newArray y random

--- game.js
+++ game.js
@@ -4,17 +4,30 @@ const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 
-
 class Juego {
     constructor(){
         this.inicializar()
+        this.generarSecuencia()
     }
 
     inicializar(){
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste: celeste,
+            // Tambien se puede hacer lo siguiente
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    //Arreglo de 10 ceros y luego se llenan de una valor entre cero y 4 redondeado
+    generarSecuencia(){
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4) )
     }
 }
 
 function empezarJuego(){
-    var juego = new Juego()
+    window.juego = new Juego()
 }
