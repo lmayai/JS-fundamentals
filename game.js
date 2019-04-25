7af05e257ea4727827f524b819d9bb3b93c0058c
lmayai <leonardomayairegui@gmail.com>
2019-04-25 12:02:38 -0500

    Verificar color elegido

--- game.js
+++ game.js
@@ -3,17 +3,20 @@ const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
-
+const ULTIMO_NIVEL = 10
 class Juego {
     constructor(){
         this.inicializar()
         this.generarSecuencia()
-        this.siguienteNivel()
+        setTimeout(()=>{
+            this.siguienteNivel()
+        },500)
     }
 
     inicializar(){
         // Tambien se puede atar de la siguiente forma
         this.elegirColor = this.elegirColor.bind(this)
+        this.siguienteNivel = this.siguienteNivel.bind(this) //Evitar que cambie el contexto de this
         btnEmpezar.classList.add('hide')
         this.nivel = 1
         this.colores = {
@@ -27,10 +30,11 @@ class Juego {
 
     //Arreglo de 10 ceros y luego se llenan de una valor entre cero y 4 redondeado
     generarSecuencia(){
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4) )
+        this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random()*4) )
     }
 
     siguienteNivel(){
+        this.subnivel = 0
         this.iluminarSecuencia()
         this.agregarEventosClick()
     }
@@ -45,6 +49,19 @@ class Juego {
                 return 'naranja';
             case 3:
                 return 'verde';
+        }
+    }
+
+    transformarColorANumero(color){
+        switch(color){
+            case 'celeste':
+                return 0;
+            case 'violeta':
+                return 1;
+            case 'naranja':
+                return 2;
+            case 'verde':
+                return 3;
         }
     }
 
@@ -85,13 +102,45 @@ class Juego {
         this.colores.verde.addEventListener('click',this.elegirColor)
     }
 
+    eliminarEventosClick(){
+        this.colores.celeste.removeEventListener('click',this.elegirColor)
+        this.colores.violeta.removeEventListener('click',this.elegirColor)
+        this.colores.naranja.removeEventListener('click',this.elegirColor)
+        this.colores.verde.removeEventListener('click',this.elegirColor)
+    }
+
     /**
      * Los manejadores de eventos se llaman con el parametro event.
      * Perderá e contexto un poco JS. 
      * Ahora this es el boton al que se llama. Es quien dispara el evento
      */
     elegirColor(event){
-        console.log(this) // Primero this fue el botón, pero con el bind se enlaza al objeto this de la clase Juego
+        //console.log(this) // Primero this fue el botón, pero con el bind se enlaza al objeto this de la clase Juego
+        //console.log(event) //En target se diferencia cual fue tocado. Y con dataSet el color
+        // Con la prop en html data-color, se setea el dataSet dentro del evento
+        // 1. Se mira el color que fue tocado
+        const nombreColor = event.target.dataset.color
+        const numeroColor = this.transformarColorANumero(nombreColor)
+        this.iluminarColor(nombreColor)
+        if (numeroColor === this.secuencia[this.subnivel]){
+            this.subnivel++;
+            if(this.subnivel===this.nivel){ //Pasa de nivel
+                this.nivel++;
+                this.eliminarEventosClick();
+                if (this.nivel === (ULTIMO_NIVEL+1)){
+                    //Ganó
+                }else{
+                    //Avanza nivel
+                    //setTimeout( () => this.siguienteNivel() , 2000) 
+                    //Tambien así, perdo cambia el this. por tal tocxa ponerlo con el bindthis
+                    setTimeout( this.siguienteNivel, 1500) 
+                    console.log('Avanza nivel' ,this.nivel)
+                }
+            } 
+        } else{ //Si toco un número que no era
+            //Perdió
+            console.log(`Pierde en nivel ${this.nivel}`)
+        }
     }
 
 }
