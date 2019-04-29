2692a755711b67ee1efbe11ecff29b944c2a4e73
lmayai <leonardomayairegui@gmail.com>
2019-04-29 10:49:30 -0500

    Usos de punto y coma

--- comp8-puntoycoma.js
+++ comp8-puntoycoma.js
@@ -1,4 +1,46 @@
 /**
  * DONDE POR PUNTO Y COMO y CUANDO NO
  * Hay casos en que si debe usarse
+*/
+
+//Innecesario, desues de una llave
+function a() {  
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+    }; //Pa que?
+}; //Pa qué?
+
+// Obligatorios en 2 casos
+console.log('HOla')
+//[1,2,3].forEach (n=>console.log(n*2))
+//Esto da un error el cual es dificil de detectar
+//Falta el ; al principio
+;[1,2,3].forEach (n=>console.log(n*2))
+
+const nombre='aaa'
+console.log('Hola J')
+//`${nombre} es un dev`
+//Tambien un error muy extraño diciendo que lod no es una funcion
+;`${nombre} es un dev`
+
+
+/**OTRO 
+function calcDoble(n) {
+    return 
+        {
+            original:n,doble:n*2
+        }
+}
+}*/
+// SI despuesdel de return encuentra un enter , lo toma como ;}
+// Por tal da error, deberia ser assí
+function calcDoble(n) {
+    return {original:n,doble:n*2}
+}
+calcDoble(2)
+
+/**
+ * SON CASOS MUY ESPECIFICOS
  */
+
+
